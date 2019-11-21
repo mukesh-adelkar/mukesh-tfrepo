b31b55071cadf964d347492b777750ce267e3722
mukesh.adelkar <mukesh.adelkar@mukeshalkarsmbp.clients.asia-pac.shell.com>
2019-11-21 11:13:01 +0530

    initial commit?

--- tf-module-test.tf
+++ tf-module-test.tf
@@ -1,15 +1,9 @@
 variable "mukesh-var-001" {
-  value = "this variable is for terraform modules training"
+  default = "this variable is for terraform modules training"
 }
 
-module "mukesh-tf-001" {
-  source  = "https://github.com/YogendraKS/git/main.tf"
-  
-  output "mukesh-tf-001" {
-      value = var.yogendra_message
-  }
-}
+
 
 output "result_mukesh" {
-  value = var.mukesh-tf-001
+  value = var.mukesh-var-001
 }
